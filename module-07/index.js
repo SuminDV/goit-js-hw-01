7d006466bba89082372259ac022e391b10eccd77
SuminDV <sumin.dmytro@gmail.com>
2019-10-22 13:50:26 +0300

    update 2.0

--- module-07/index.js
+++ module-07/index.js
@@ -69,32 +69,32 @@
 
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
-// const images = [
-//   {
-//     url:
-//       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url:
-//       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url:
-//       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
 
-// const ul = document.querySelector('#gallery');
-// let imgStr = '';
-// images.forEach(el => {
-//   const img = document.createElement('img');
-//   // img.src = el.url;
-//   imgStr += `<li><img src=${el.url} alt=${el.alt}></li>`;
-// });
-// ul.insertAdjacentHTML('afterbegin', imgStr)
+const ul = document.querySelector('#gallery');
+let imgStr = '';
+images.forEach(el => {
+  const img = document.createElement('img');
+  // img.src = el.url;
+  imgStr += `<li><img src=${el.url} alt=${el.alt}></li>`;
+});
+ul.insertAdjacentHTML('afterbegin', imgStr)
 
 // Задание 4
 // Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
